c2e287aa1f8e8a49f830fc63deb629435411e9a6
chinmaywadhwa007 <cwadhwa018@gmail.com>
2025-03-25 17:22:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("hello my name is chimmay wadhwa ");
+
+
+console.log("my name is chinmay wadhwa ");
+console.log("i am from jecrc college where i studied engineerring as my degreee");
+console.log("after that i have started working with kvon tech as an inter for now");
+
+
+var a=prompt("enter any number between 1 to 1000");
+var istrue =con("are u sure do you want to blast your laptop by exit this page thing twice before leavaing my side ");
+
+
+if(istrue){
+    console.log("your pc will be blast within 10 sec...!")
+}
+else{
+    console.log("your pc will bs saved is you stay in the website for a while then u can close")
+}
+
+
+console.log("your number is "+a);
