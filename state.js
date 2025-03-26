711843a663ecf6c2d869624489abd50f708b6ecc
chinmaywadhwa007 <cwadhwa018@gmail.com>
2025-03-26 16:10:39 +0530

    Add files via upload

---
+++ state.js
@@ -0,0 +1,51 @@
+// JavaScript statements are programming instructions that a computer executes. A computer program is essentially a list of these “instructions” designed to perform tasks. In a programming language, such instructions are called statements.
+
+
+// types of statements as follow 
+
+// 1 control flow statements 
+// let number =200;
+// if(number>100){
+//     console.log("number is bigger then the given number ");
+
+
+
+// 2 function declerations 
+// A function declaration is a statement that defines a function in JavaScript. Functions are reusable blocks of code designed to perform specific tasks.
+
+// function greet(name){
+//     return "helllo,"+name;
+
+
+// }
+// console.log(greet("hellow my name pm modi "));
+
+
+// 3. Return Statement
+// The return statement is used to exit a function and optionally pass a value back to the calling code.
+
+// function add(x, y) {
+//     return x + y;
+// }
+// let result = add(5, 3); // result will be 8
+
+// 4. Throw Statement
+// The throw statement is used to create custom errors in JavaScript. It is often used in conjunction with try…catch to handle errors.
+
+// function checkage(age){
+//     if (age<18){
+//         throw new Error("age must be 18 plus to get the driving lisance ");
+        
+//     }
+// }
+
+// 5 break and continue statement
+// the break an cont. statement are used within loops break eists the loop to skip to the next iterations 
+// example we can see here as follow 
+
+// for (let i = 0; i < 100; i++) {
+//     if (i === 50) {
+//         break;  // Exits the loop when i equals 5
+//     }
+//     console.log(i);
+// }
