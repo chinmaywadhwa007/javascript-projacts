2b45187490904e592b4b2a61a66200e8f9b1dfbd
chinmaywadhwa007 <cwadhwa018@gmail.com>
2025-03-26 10:15:03 +0530

    Add files via upload
    in this there are the oprators examples

---
+++ link.js
@@ -0,0 +1,41 @@
+// let a;
+// a=5 + 4 * 4;
+
+// console.log(a); primative oprrator statment 
+
+
+
+
+// this is for logical operator &&
+
+// let age = 2;
+// let idProof = true;
+
+
+// if (age >= 18 && idProof) {
+//   console.log("Allowed"); 
+// } else {
+//   console.log("Not Allowed");  
+// }
+
+// this operator is || means or
+
+// Check if at least one condition is true
+
+// let age = 16;
+// let hasGuardian = true;
+
+//  Logical OR checks if either condition is true
+
+// if (age >= 18 || hasGuardian) {
+//   console.log("Allowed");
+// } else {
+//   console.log("Not Allowed");
+// }
+
+// . Logical NOT (!) Operator
+// The logical NOT (!) operator (inverts) the boolean value of its operand. If the operand is true, it returns false. If the operand is false, it returns true.
+
+// let isAllowed = false;
+
+// console.log(!isAllowed); // means it change the value which we have entered as variables
